9b0c901cc58f0d639979e8b5957eba49afa55c31
dominc8 <kormandominik@gmail.com>
2020-05-02 19:43:22 +0200

    Added naive implementation of reading pin states E2-E5 and sending them through uart

--- device/F429LA/Core/Src/main.c
+++ device/F429LA/Core/Src/main.c
@@ -563,16 +563,24 @@ static void GPIO_Init(void)
 
     /* GPIOG Clock Enable */
     __HAL_RCC_GPIOG_CLK_ENABLE();
+    __HAL_RCC_GPIOE_CLK_ENABLE();
 
-    /*Configure GPIO pin Output Level */
+    /* Configure GPIO pin Output Level */
     HAL_GPIO_WritePin(GPIOG, LD3_Pin|LD4_Pin, GPIO_PIN_RESET);
 
-    /*Configure GPIO pins : LD3_Pin LD4_Pin */
+    /* Configure GPIO pins : LD3_Pin LD4_Pin */
     GPIO_InitStruct.Pin     = LD3_Pin|LD4_Pin;
     GPIO_InitStruct.Mode    = GPIO_MODE_OUTPUT_PP;
     GPIO_InitStruct.Pull    = GPIO_NOPULL;
     GPIO_InitStruct.Speed   = GPIO_SPEED_FREQ_LOW;
     HAL_GPIO_Init(GPIOG, &GPIO_InitStruct);
+
+    /* Configure GPIO pins for sampling data (E2-E5) */
+    GPIO_InitStruct.Pin = (uint16_t)0x003C;
+    GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
+    HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
 }
 
  /**
--- device/F429LA/Core/Src/samplingtask.c
+++ device/F429LA/Core/Src/samplingtask.c
@@ -17,12 +17,26 @@ extern UART_HandleTypeDef huart1;
 
 void StartSamplingTask(void const * arg)
 {
-	uint8_t pData[] = { 'H', 'e', 'l', 'l', 'o', '!', '\r', '\n'};
+	/* uint8_t pData[] = { 'H', 'e', 'l', 'l', 'o', '!', '\r', '\n'}; */
+
+#define SAMPLES		1000
+
+	volatile static uint8_t samples[SAMPLES];
+	volatile uint8_t *sample_ptr = samples;
 	for(;;)
     {
+        /*
         osDelay(1000);
         HAL_GPIO_TogglePin(GPIOG, GREEN_LED_Pin);
         HAL_UART_Transmit(&huart1, pData, 8, 100);
+        */
+		while (sample_ptr < samples + SAMPLES)
+		{
+			*sample_ptr = (uint8_t)((GPIOE->IDR) & 0x3C);
+			++sample_ptr;
+		}
+		sample_ptr = samples;
+		HAL_UART_Transmit(&huart1, samples, SAMPLES, 1000);
     }
 }
 
