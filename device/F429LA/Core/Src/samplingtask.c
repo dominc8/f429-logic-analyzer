02df8014e8f9b324f4e01b1c1794fc9616f6bff1
dominc8 <kormandominik@gmail.com>
2020-05-03 17:31:26 +0200

    DMA working but memory pointer is not being incremented

--- device/F429LA/Core/Src/samplingtask.c
+++ device/F429LA/Core/Src/samplingtask.c
@@ -14,7 +14,8 @@
 
 extern UART_HandleTypeDef huart1;
 
-
+#define ARRAY_SIZE	100
+volatile static uint8_t array[ARRAY_SIZE] = { 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA};
 
 
 
@@ -50,38 +51,41 @@ void StartSamplingTask(void const * arg)
 }
 
 
-TIM_HandleTypeDef		htim7;
+TIM_HandleTypeDef		htim8;
 
+DMA_HandleTypeDef		hdma2;
 
 
 HAL_StatusTypeDef SamplingTask_HALInit(void)
 {
+#if 0
   RCC_ClkInitTypeDef    clkconfig;
   uint32_t              uwTimclock = 0;
   uint32_t              uwPrescalerValue = 0;
   uint32_t              pFLatency;
+  HAL_StatusTypeDef		retval = HAL_ERROR;
 
-  /*Configure the TIM7 IRQ priority */
-  HAL_NVIC_SetPriority(TIM7_IRQn, 10,0);
+  /*Configure the TIM8 IRQ priority */
+  HAL_NVIC_SetPriority(TIM8_UP_TIM13_IRQn , 10,0);
 
-  /* Enable the TIM7 global Interrupt */
-  HAL_NVIC_EnableIRQ(TIM7_IRQn);
+  /* Enable the TIM8 global Interrupt */
+  HAL_NVIC_EnableIRQ(TIM8_UP_TIM13_IRQn );
 
-  /* Enable TIM7 clock */
-  __HAL_RCC_TIM7_CLK_ENABLE();
+  /* Enable TIM8 clock */
+  __HAL_RCC_TIM8_CLK_ENABLE();
 
   /* Get clock configuration */
   HAL_RCC_GetClockConfig(&clkconfig, &pFLatency);
 
-  /* Compute TIM7 clock 2*? */
+  /* Compute TIM8 clock */
   uwTimclock = 2 * HAL_RCC_GetPCLK1Freq();
 
-  /* Compute the prescaler value to have TIM7 counter clock equal to 1MHz */
+  /* Compute the prescaler value to have TIM8 counter clock equal to 1MHz */
   // uwPrescalerValue = 0;
   uwPrescalerValue = 7200;
 
   /* Initialize TIM6 */
-  htim7.Instance = TIM7;
+  htim8.Instance = TIM8;
 
   /* Initialize TIMx peripheral as follow:
   + Period = [(TIM6CLK/1000) - 1]. to have a (1/1000) s time base.
@@ -90,38 +94,119 @@ HAL_StatusTypeDef SamplingTask_HALInit(void)
   + Counter direction = Up
   */
   // htim7.Init.Period = (uwTimclock / 1000000) - 1;
-  htim7.Init.Period = 9999;
-  htim7.Init.Prescaler = uwPrescalerValue;
-  htim7.Init.ClockDivision = 0;
-  htim7.Init.CounterMode = TIM_COUNTERMODE_UP;
-  if(HAL_TIM_Base_Init(&htim7) == HAL_OK)
+  htim8.Init.Period = 9999;
+  htim8.Init.Prescaler = uwPrescalerValue;
+  htim8.Init.ClockDivision = 0;
+  htim8.Init.CounterMode = TIM_COUNTERMODE_UP;
+  if(HAL_TIM_Base_Init(&htim8) == HAL_OK)
   {
     /* Start the TIM time Base generation in interrupt mode */
-    return HAL_TIM_Base_Start_IT(&htim7);
+    retval = HAL_TIM_Base_Start_IT(&htim8);
   }
+  // dma2 ch7str1
 
   /* Return function status */
-  return HAL_ERROR;
+  return retval;
+#else
+  RCC_ClkInitTypeDef    clkconfig;
+  uint32_t              uwTimclock = 0;
+  uint32_t              uwPrescalerValue = 0;
+  uint32_t              pFLatency;
+  HAL_StatusTypeDef		retval = HAL_ERROR;
+
+  /*Configure the TIM8 IRQ priority */
+  HAL_NVIC_SetPriority(TIM8_UP_TIM13_IRQn , 10,0);
+  HAL_NVIC_SetPriority(DMA2_Stream1_IRQn , 9,0);
+  HAL_NVIC_EnableIRQ(TIM8_UP_TIM13_IRQn );
+  HAL_NVIC_EnableIRQ(DMA2_Stream1_IRQn );
+
+  /* Enable TIM8 clock */
+  __HAL_RCC_TIM8_CLK_ENABLE();
+  __HAL_RCC_DMA2_CLK_ENABLE();
+
+  /* Get clock configuration */
+  HAL_RCC_GetClockConfig(&clkconfig, &pFLatency);
+
+  /* Compute TIM8 clock */
+  uwTimclock = 2 * HAL_RCC_GetPCLK1Freq();
+
+  /* Compute the prescaler value to have TIM8 counter clock equal to 1MHz */
+  // uwPrescalerValue = 0;
+  uwPrescalerValue = 7200;
+
+  /* Initialize TIM6 */
+  htim8.Instance = TIM8;
+
+  /* Initialize TIMx peripheral as follow:
+  + Period = [(TIM6CLK/1000) - 1]. to have a (1/1000) s time base.
+  + Prescaler = (uwTimclock/1000000 - 1) to have a 1MHz counter clock.
+  + ClockDivision = 0
+  + Counter direction = Up
+  */
+  // htim7.Init.Period = (uwTimclock / 1000000) - 1;
+  htim8.Init.Period = 9999;
+  htim8.Init.Prescaler = uwPrescalerValue;
+  htim8.Init.ClockDivision = 0;
+  htim8.Init.CounterMode = TIM_COUNTERMODE_UP;
+
+
+
+
+  if(HAL_TIM_Base_Init(&htim8) == HAL_OK)
+  {
+	  /* 14 - trigger dma, 8 - update dma*/
+	  TIM8->DIER = (uint16_t)(0x0101);
+
+	  TIM8->CR1 |= 1;
+
+	  retval = HAL_OK;
+  }
+  // dma2 ch7str1
+  /* Clear Interrupt Status Bits */
+  DMA2->LIFCR |= (uint32_t)(0x00000F40);
+
+  DMA2_Stream1->CR = DMA_CHANNEL_7
+                   | DMA_PRIORITY_VERY_HIGH
+                   | DMA_MINC_ENABLE
+				   | DMA_PINC_ENABLE
+				   | DMA_CIRCULAR;
+
+  /* Size of data transfer */
+  DMA2_Stream1->NDTR = 1;
+
+  DMA2_Stream1->PAR = (uint32_t)(0x40021010);
+  DMA2_Stream1->M0AR = (uint32_t)(array);
+
+  DMA2_Stream1->CR |= 1;
+
+  return retval;
+#endif
 }
 
-void TIM7_IRQHandler(void)
+void TIM8_UP_TIM13_IRQHandler(void)
 {
-    if (__HAL_TIM_GET_FLAG(&htim7, TIM_FLAG_UPDATE) != RESET)      //In case other interrupts are also running
+    if (__HAL_TIM_GET_FLAG(&htim8, TIM_FLAG_UPDATE) != RESET)      //In case other interrupts are also running
 
     {
 
-        if (__HAL_TIM_GET_ITSTATUS(&htim7, TIM_IT_UPDATE) != RESET)
+        if (__HAL_TIM_GET_ITSTATUS(&htim8, TIM_IT_UPDATE) != RESET)
 
         {
 
-            __HAL_TIM_CLEAR_FLAG(&htim7, TIM_FLAG_UPDATE);
-
+            __HAL_TIM_CLEAR_FLAG(&htim8, TIM_FLAG_UPDATE);
+            array[0] = 0xAA;
             HAL_GPIO_TogglePin(GPIOG, GREEN_LED_Pin);
             /*put your code here */
 
         }
 
     }
+
+}
+
+void DMA2_Stream1_IRQHandler(void)
+{
+	array[10] = 99;
 
 }
 
