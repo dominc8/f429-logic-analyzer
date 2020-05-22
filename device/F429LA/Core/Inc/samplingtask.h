e52b2e53072af44e915e42549ba39ea0dd9db287
dominc8 <kormandominik@gmail.com>
2020-05-22 18:31:39 +0200

    Added manual interrupt sampling and not yet working uart with dma

--- device/F429LA/Core/Inc/samplingtask.h
+++ device/F429LA/Core/Inc/samplingtask.h
@@ -11,6 +11,17 @@
 #include "stm32f4xx_hal.h"
 #include "stm32f4xx_hal_tim.h"
 
+#define SAMPLES_SIZE		10000
+
+typedef struct Sample_Data_Tag Sample_Data_T;
+
+struct Sample_Data_Tag
+{
+	uint8_t data[SAMPLES_SIZE];
+	Sample_Data_T *next_data;
+};
+extern Sample_Data_T sample_arr[2];
+
 void StartSamplingTask(void const * arg);
 HAL_StatusTypeDef SamplingTask_HALInit(void);
 
--- device/F429LA/Core/Src/samplingtask.c
+++ device/F429LA/Core/Src/samplingtask.c
@@ -8,34 +8,42 @@
 #include <stdint.h>
 
 #include "samplingtask.h"
-#include "cmsis_os.h"
 
 #define GREEN_LED_Pin GPIO_PIN_13
+#define RED_LED_Pin   GPIO_PIN_14
+
 
 extern UART_HandleTypeDef huart1;
 
-#define ARRAY_SIZE	100
-volatile static uint8_t array[ARRAY_SIZE] = { 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA};
 
+Sample_Data_T sample_arr[2] = {{.data = {0}, .next_data = &sample_arr[1] }, {.data = {0}, .next_data = &sample_arr[0]}};
+
+static volatile Sample_Data_T *curr_sample = sample_arr;
+
+volatile uint8_t *sample_ptr = sample_arr[0].data;
+
+static volatile uint8_t ready_to_transmission = 0;
+
+
+static volatile uint32_t *sdram_ptr = (uint32_t *)0xD0000000;
 
 void StartSamplingTask(void const * arg)
 {
 
-#define SAMPLES		1000
 
-	volatile static uint8_t samples[SAMPLES];
-	volatile uint8_t *sample_ptr = samples;
+	// *sdram_ptr = (uint32_t)0x12345678;
+	//HAL_GPIO_TogglePin(GPIOG, RED_LED_Pin);
 
 	for(;;)
     {
-		while (sample_ptr < samples + SAMPLES)
-		{
-			*sample_ptr = (uint8_t)((GPIOE->IDR) & 0x3C);
-			++sample_ptr;
-		}
-		sample_ptr = samples;
-		HAL_UART_Transmit(&huart1, samples, SAMPLES, 1000);
-		array[4] = 0xAA;
+		while (ready_to_transmission == 0);
+		ready_to_transmission = 0;
+		DMA2_Stream7->CR |= DMA_SxCR_EN;
+		// HAL_UART_Transmit(&huart1, samples, SAMPLES_SIZE_2, 1000);
+
+		//if ((*sdram_ptr) == 0x12345678)
+		HAL_GPIO_TogglePin(GPIOG, RED_LED_Pin);
+
     }
 }
 
@@ -51,73 +59,73 @@ HAL_StatusTypeDef SamplingTask_HALInit(void)
   HAL_StatusTypeDef		retval = HAL_ERROR;
 
   /* Configure the IRQs */
-  // HAL_NVIC_SetPriority(TIM8_UP_TIM13_IRQn , 10,0);
-  // HAL_NVIC_SetPriority(DMA2_Stream1_IRQn , 9,0);
-  // HAL_NVIC_EnableIRQ(TIM8_UP_TIM13_IRQn );
+  HAL_NVIC_SetPriority(TIM8_UP_TIM13_IRQn , 10, 0);
+  // HAL_NVIC_SetPriority(DMA2_Stream7_IRQn , 9,0);
+  HAL_NVIC_EnableIRQ(TIM8_UP_TIM13_IRQn);
   // HAL_NVIC_EnableIRQ(DMA2_Stream1_IRQn );
 
   /* Enable clocks */
   __HAL_RCC_TIM8_CLK_ENABLE();
-  __HAL_RCC_DMA2_CLK_ENABLE();
+  // __HAL_RCC_DMA2_CLK_ENABLE();
 
   /* Get clock configuration */
   HAL_RCC_GetClockConfig(&clkconfig, &pFLatency);
 
   /* Compute TIM8 parameters */
   uwTimclock = 2 * HAL_RCC_GetPCLK1Freq();
-  uwPrescalerValue = 7200;
+  uwPrescalerValue = 72;
 
   /* Initialize TIM8 */
   htim8.Instance = TIM8;
 
-  htim8.Init.Period = 9999;
+  htim8.Init.Period = 99;
   htim8.Init.Prescaler = uwPrescalerValue;
   htim8.Init.ClockDivision = 0;
   htim8.Init.CounterMode = TIM_COUNTERMODE_UP;
 
   if(HAL_OK == HAL_TIM_Base_Init(&htim8))
   {
-	  /* 8 - update dma*/
-	  TIM8->DIER = (uint16_t)(1 << 8);
-
+	  HAL_TIM_Base_Start_IT(&htim8);
 	  retval = HAL_OK;
   }
 
-  DMA2_Stream1->CR = 0;									/* Turn off DMA2 Stream1 */
-  DMA2->LIFCR |= (uint32_t)(0x00000F40);				/* Clear Interrupt Status Bits */
-  DMA2_Stream1->PAR = (volatile uint32_t)(0x40021010);	/* Set Peripheral (Source) Address */
-  DMA2_Stream1->M0AR = (volatile uint32_t)(array + 4);	/* Set Memory (Destination) Address */
-  DMA2_Stream1->NDTR = 4;								/* Size of single trasfer */
-
-  DMA2_Stream1->CR = DMA_CHANNEL_7
-                   | DMA_PRIORITY_HIGH
-                   | DMA_MINC_ENABLE
-				   | DMA_PINC_ENABLE
-				   | DMA_CIRCULAR;;
-
-  /* TODO: add FIFO if too big load */
-
   if (HAL_OK == retval)
   {
-	  /* Turn on DMA2 Stream1 and TIM8 */
-	  DMA2_Stream1->CR |= 1;
 	  TIM8->CR1 |= 1;
   }
 
   return retval;
 }
 
+
+
+
 void TIM8_UP_TIM13_IRQHandler(void)
 {
+	static uint8_t move_mask = 4;
     if (__HAL_TIM_GET_FLAG(&htim8, TIM_FLAG_UPDATE) != RESET)      //In case other interrupts are also running
     {
         if (__HAL_TIM_GET_ITSTATUS(&htim8, TIM_IT_UPDATE) != RESET)
         {
             __HAL_TIM_CLEAR_FLAG(&htim8, TIM_FLAG_UPDATE);
-            array[0] = 0xAA;
-            HAL_GPIO_TogglePin(GPIOG, GREEN_LED_Pin);
+            /* Get pin values from E2-E5 and B12-B15 */
+            *sample_ptr = (uint8_t)( ((GPIOE->IDR & 0x003C) >> 2) | ((GPIOB->IDR & 0xF000) >> 8));
+            sample_ptr++;
+            if (sample_ptr >= curr_sample->data + SAMPLES_SIZE)
+            {
+            	ready_to_transmission = 1;
+            	curr_sample = curr_sample->next_data;
+            	sample_ptr = curr_sample->data;
+            	HAL_GPIO_TogglePin(GPIOG, GREEN_LED_Pin);
+            }
         }
     }
+}
+
+void EXTI0_IRQHandler(void)
+{
+	__HAL_GPIO_EXTI_CLEAR_FLAG(1);
+	HAL_GPIO_TogglePin(GPIOG, RED_LED_Pin);
 }
 
 
