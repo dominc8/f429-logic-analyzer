00244ea99c7f0141ee078aaf80637a0dfd11be22
dominc8 <kormandominik@gmail.com>
2020-08-17 20:38:11 +0200

    Handle configuration of number of sampling sources

--- device/F429LA/Core/Src/main.c
+++ device/F429LA/Core/Src/main.c
@@ -418,15 +418,9 @@ Command_T ParseCommand(Config_T *config)
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
-    /* USER CODE BEGIN Callback 0 */
-
-    /* USER CODE END Callback 0 */
     if (htim->Instance == TIM6) {
       HAL_IncTick();
     }
-    /* USER CODE BEGIN Callback 1 */
-
-    /* USER CODE END Callback 1 */
 }
 
 
@@ -444,18 +438,7 @@ void DMA2_Stream5_IRQHandler(void)
 }
 
 #ifdef  USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
 void assert_failed(uint8_t *file, uint32_t line)
 { 
-    /* USER CODE BEGIN 6 */
-    /* User can add his own implementation to report the file name and line number,
-       tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-    /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
--- device/F429LA/Core/Src/sampling.c
+++ device/F429LA/Core/Src/sampling.c
@@ -16,25 +16,47 @@ static volatile uint8_t *sample_ptr = sample_arr[0].data;
 
 static volatile uint8_t ready_to_transmission = 0;
 static volatile uint8_t sampling_enabled = 0;
+static volatile uint8_t sample_ptr_offset = 0;
 
 static volatile uint32_t *sdram_ptr = (uint32_t *)0xD0000000;
 
 static void RunSampling_RT(void);
 static void RunSampling_NRT(void);
+static void SampleSrc_1(void);
+static void SampleSrc_2(void);
+static void SampleSrc_4(void);
+static void SampleSrc_8(void);
 
+static void (*sample_func_ptr)(void) = &SampleSrc_8;
 
 void StartSampling(Config_T *config)
 {
     USART1->BRR = UART_BRR_SAMPLING16(HAL_RCC_GetPCLK2Freq(), config->baudrate);
 
-    // TODO: Change pointer to function to be called in interrupt
-    // Configure sampling sources
+    switch (config->sampling_sources)
+    {
+        case SamplingSources_ONE:
+            sample_func_ptr = &SampleSrc_1;
+            break;
+        case SamplingSources_TWO:
+            sample_func_ptr = &SampleSrc_2;
+            break;
+        case SamplingSources_FOUR:
+            sample_func_ptr = &SampleSrc_4;
+            break;
+        case SamplingSources_EIGHT:
+        default:
+            sample_func_ptr = &SampleSrc_8;
+            break;
+    }
 
-#if 1
     sampling_enabled = 1;
+    sample_ptr_offset = 0;
+    curr_sample = sample_arr;
+    sample_ptr = curr_sample->data;
 
     /* Reset and restart timer */
-    TIM8->ARR = 2 * HAL_RCC_GetPCLK1Freq() / config->sampling_freq; // ?
+    TIM8->ARR = 2 * HAL_RCC_GetPCLK1Freq() / config->sampling_freq;
     TIM8->CNT = 0;
     TIM8->CR1 |= TIM_CR1_CEN;
 
@@ -49,7 +71,6 @@ void StartSampling(Config_T *config)
         default:
             break;
     }
-#endif
 }
 
 HAL_StatusTypeDef Timer_Init(void)
@@ -137,26 +158,104 @@ void RunSampling_NRT(void)
 
 void TIM8_UP_TIM13_IRQHandler(void)
 {
-    static uint8_t move_mask = 4;
     if (__HAL_TIM_GET_FLAG(&htim8, TIM_FLAG_UPDATE) != RESET)      //In case other interrupts are also running
     {
         if (__HAL_TIM_GET_ITSTATUS(&htim8, TIM_IT_UPDATE) != RESET)
         {
             __HAL_TIM_CLEAR_FLAG(&htim8, TIM_FLAG_UPDATE);
-            /* Get pin values from E2-E5 and B8-B11, format: [B11 B10 B9 B8 E5 E4 E3 E2] */
-            *sample_ptr = (uint8_t)( ((GPIOE->IDR & 0x003C) >> 2) | ((GPIOB->IDR & 0x0F00) >> 4));
-            // *sample_ptr = 'A';
-            sample_ptr++;
-            if (sample_ptr >= curr_sample->data + SAMPLES_SIZE)
-            {
-                ready_to_transmission = 1;
-                curr_sample = curr_sample->next_data;
-                sample_ptr = curr_sample->data;
-                HAL_GPIO_TogglePin(GPIOG, GREEN_LED_Pin);
-            }
+            sample_func_ptr();
         }
     }
 }
+
+
+static void SampleSrc_1(void)
+{
+    *sample_ptr |= (uint8_t)((GPIOB->IDR & 0x0100) >> (sample_ptr_offset + 1));
+    if (sample_ptr_offset == 0x07)
+    {
+        sample_ptr_offset = 0;
+        sample_ptr++;
+
+        if (sample_ptr >= curr_sample->data + SAMPLES_SIZE)
+        {
+            ready_to_transmission = 1;
+            curr_sample = curr_sample->next_data;
+            sample_ptr = curr_sample->data;
+        }
+
+        *sample_ptr = 0;
+    }
+    else
+    {
+        sample_ptr_offset++;
+    }
+}
+
+static void SampleSrc_2(void)
+{
+    /* Get pin values from B8-B9, format: [B9 B8 B9 B8 B9 B8 B9 B8] */
+    *sample_ptr |= (uint8_t)((GPIOB->IDR & 0x0300) >> (sample_ptr_offset + 2));
+    if (sample_ptr_offset == 0x06)
+    {
+        sample_ptr_offset = 0;
+        sample_ptr++;
+
+        if (sample_ptr >= curr_sample->data + SAMPLES_SIZE)
+        {
+            ready_to_transmission = 1;
+            curr_sample = curr_sample->next_data;
+            sample_ptr = curr_sample->data;
+        }
+
+        *sample_ptr = 0;
+    }
+    else
+    {
+        sample_ptr_offset += 2;
+    }
+}
+
+static void SampleSrc_4(void)
+{
+    /* Get pin values from B8-B11, format: [B11 B10 B9 B8 B11 B10 B9 B8] */
+    *sample_ptr |= (uint8_t)((GPIOB->IDR & 0x0F00) >> (sample_ptr_offset + 4));
+    if (sample_ptr_offset == 0x04)
+    {
+        sample_ptr_offset = 0;
+        sample_ptr++;
+
+        if (sample_ptr >= curr_sample->data + SAMPLES_SIZE)
+        {
+            ready_to_transmission = 1;
+            curr_sample = curr_sample->next_data;
+            sample_ptr = curr_sample->data;
+        }
+
+        *sample_ptr = 0;
+    }
+    else
+    {
+        sample_ptr_offset += 4;
+    }
+}
+
+
+static void SampleSrc_8(void)
+{
+    /* Get pin values from E2-E5 and B8-B11, format: [B11 B10 B9 B8 E5 E4 E3 E2] */
+    *sample_ptr = (uint8_t)( ((GPIOE->IDR & 0x003C) >> 2) | ((GPIOB->IDR & 0x0F00) >> 4));
+    sample_ptr++;
+    if (sample_ptr >= curr_sample->data + SAMPLES_SIZE)
+    {
+        ready_to_transmission = 1;
+        curr_sample = curr_sample->next_data;
+        sample_ptr = curr_sample->data;
+    }
+}
+
+
+
 
 void EXTI0_IRQHandler(void)
 {
