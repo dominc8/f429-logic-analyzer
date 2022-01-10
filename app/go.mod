fbe14b2255d4fdc6abacd8d4356d0c35166d36f4
dominc8 <kormandominik@gmail.com>
2022-01-10 22:39:14 +0100

    Start tui

--- app/go.mod
+++ app/go.mod
@@ -2,6 +2,18 @@ module f429-logic-analyzer/app
 
 go 1.17
 
-require github.com/tarm/serial v0.0.0-20180830185346-98f6abe2eb07
+require (
+	github.com/rivo/tview v0.0.0-20220106183741-90d72bc664f5
+	github.com/tarm/serial v0.0.0-20180830185346-98f6abe2eb07
+)
 
-require golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+require (
+	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/gdamore/tcell/v2 v2.4.1-0.20210905002822-f057f0a857a1 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/term v0.0.0-20210220032956-6a3ed077a48d // indirect
+	golang.org/x/text v0.3.6 // indirect
+)
--- app/main.go
+++ app/main.go
@@ -3,11 +3,57 @@ package main
 import (
 	"time"
 	"github.com/tarm/serial"
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
     "log"
+    "fmt"
 )
 
+var (
+    baudrate int
+    sampling_freq int
+)
+
+func cmd_run() {
+    log.Printf("[STUB] Send run command")
+}
+
+func cmd_baud() {
+    log.Printf("[STUB] Send baud command")
+}
+
+func draw_text_line(screen tcell.Screen, str string, x int, y *int, width int) {
+    tview.Print(screen, str, x, *y, width, tview.AlignCenter, tcell.ColorLime)
+    *y++
+}
+
+func draw_config(screen tcell.Screen, x int, y int, width int, height int)  (int, int, int, int) {
+    y++
+
+    draw_text_line(screen, "Mode: RT", x, &y, width)
+    draw_text_line(screen, fmt.Sprintf("Baud: %v", baudrate), x, &y, width)
+    draw_text_line(screen, fmt.Sprintf("Sampling frequency: %vHz", sampling_freq), x, &y, width)
+
+    return 0, 0, 0, 0
+}
 
 func main() {
+    baudrate = 115200
+    sampling_freq = 1000
+    app := tview.NewApplication()
+    list := tview.NewList().
+                AddItem("Start sampling", "", 'a', cmd_run).
+                AddItem("Set baud rate", "", 'b', cmd_baud).
+                AddItem("Quit", "", 'q', func() {
+                    app.Stop()
+                })
+    flex := tview.NewFlex().
+                AddItem(list, 0, 1, true).
+                AddItem(tview.NewBox().SetBorder(true).SetDrawFunc(draw_config), 0, 4, false)
+    if err := app.SetRoot(flex, true).SetFocus(flex).Run(); err != nil {
+        panic(err)
+    }
+    return
     c := &serial.Config{Name: "/dev/ttyACM0", Baud: 115200, ReadTimeout: time.Millisecond * 5000}
     s, err := serial.OpenPort(c)
     if err != nil {
