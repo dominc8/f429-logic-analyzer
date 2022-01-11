d52d95565d9f94e78b6584f94258c1d3785c9f97
dominc8 <kormandominik@gmail.com>
2022-01-11 21:49:38 +0100

    First working version or it seems, dumped binary data to be verified

--- app/main.go
+++ app/main.go
@@ -1,34 +1,35 @@
 package main
 
 import (
-	"time"
-	"github.com/tarm/serial"
-	"github.com/rivo/tview"
+    "github.com/tarm/serial"
+    "github.com/rivo/tview"
     "log"
     "strconv"
     "unicode"
+    "fmt"
+    "os"
+    "encoding/binary"
 )
 
 
 type device_config struct {
-    mode string
+    mode int
     baudrate int
     sampling_freq int
+    sampling_sources int
 }
 
 var (
+    log_file *os.File
+    serial_port *serial.Port
     config device_config
     app *tview.Application
     list *tview.List
     form *tview.Form
 )
 
-func cmd_run() {
-    log.Printf("[STUB] Send run command")
-}
-
 func mode_select_callback(option string, optionIndex int) {
-    config.mode = option
+    config.mode = optionIndex
 }
 
 func baud_select_callback(text string) {
@@ -39,8 +40,29 @@ func sampling_freq_select_callback(text string) {
     config.sampling_freq, _ = strconv.Atoi(text)
 }
 
+func sampling_sources_select_callback(option string, optionIndex int) {
+    config.sampling_sources, _ = strconv.Atoi(option)
+}
+
+func send_command(text string) {
+    cmd := []byte(fmt.Sprintf("%-20s", text))
+    n, err := serial_port.Write(cmd)
+    if err != nil {
+        log.Printf("cmd '%s' [%v] failed: %v", text, cmd, err)
+    }
+    log.Printf("Sent cmd '%v' [%v]", string(cmd), n)
+}
+
+func cmd_run() {
+    send_command("run")
+    go read_data()
+}
+
 func update_config() {
-    log.Printf("[STUB] %v %v %v", config.mode, config.baudrate, config.sampling_freq)
+    send_command(fmt.Sprintf("mode %v", config.mode))
+    send_command(fmt.Sprintf("baud %v", config.baudrate))
+    send_command(fmt.Sprintf("freq %v", config.sampling_freq))
+    send_command(fmt.Sprintf("src %v", config.sampling_sources))
     app.SetFocus(list)
 }
 
@@ -48,11 +70,42 @@ func validate_unsigned_int(testToCheck string, lastChar rune) bool {
     return unicode.IsDigit(lastChar)
 }
 
+func connect_to_device() {
+    serial_config := &serial.Config{Name: "/dev/ttyACM0", Baud: 115200}
+    s, err := serial.OpenPort(serial_config)
+    if err != nil {
+        log.Fatal(err)
+    }
+    log.Printf("Serial port opened %v", s)
+    serial_port = s
+}
+
+func read_data() {
+    outf, _ := os.Create("tmp_data.bin")
+    defer outf.Close()
+    buf := make([]byte, 256)
+
+    for {
+        n, err := serial_port.Read(buf)
+        if err == nil && n > 0 {
+            binary.Write(outf, binary.LittleEndian, buf[:n])
+        } else {
+            break
+        }
+    }
+}
 
 func main() {
-    config.mode = "RT"
+    config.mode = 0
     config.baudrate = 115200
     config.sampling_freq = 1000
+    config.sampling_sources = 8
+
+    log_file, _ = os.OpenFile("log.txt", os.O_APPEND|os.O_RDWR|os.O_CREATE, 0644)
+    defer log_file.Close()
+    log.SetOutput(log_file)
+    log.SetFlags(log.Lshortfile|log.LstdFlags)
+
     app = tview.NewApplication()
     list = tview.NewList().
                 AddItem("Start sampling", "", 'a', cmd_run).
@@ -64,52 +117,25 @@ func main() {
                 })
 
     form = tview.NewForm().
-		AddDropDown("Mode", []string{"RT", "NRT"}, 0, mode_select_callback).
-		AddInputField("Baud rate", "115200", 10, validate_unsigned_int, baud_select_callback).
-		AddInputField("Sampling frequency", "1000", 10, validate_unsigned_int, sampling_freq_select_callback).
-		AddButton("Save", update_config).
-        SetCancelFunc(func() {
-            app.SetFocus(list)
-        })
+                AddDropDown("Mode", []string{"RT", "NRT"}, 0, mode_select_callback).
+                AddInputField("Baud rate", "115200", 10, validate_unsigned_int, baud_select_callback).
+                AddInputField("Sampling frequency", "1000", 10, validate_unsigned_int, sampling_freq_select_callback).
+                AddDropDown("Sampling sources", []string{"1", "2", "4", "8"}, 3, sampling_sources_select_callback).
+                AddButton("Save", update_config).
+                SetCancelFunc(func() {
+                    app.SetFocus(list)
+                })
 
-	form.SetBorder(true).SetTitle("Configuration").SetTitleAlign(tview.AlignCenter)
+    form.SetBorder(true).SetTitle("Configuration").SetTitleAlign(tview.AlignCenter)
 
     flex := tview.NewFlex().
                 AddItem(list, 0, 1, true).
                 AddItem(form, 0, 4, false)
 
+    connect_to_device()
+
     if err := app.SetRoot(flex, true).SetFocus(flex).Run(); err != nil {
         panic(err)
     }
-    return
-    c := &serial.Config{Name: "/dev/ttyACM0", Baud: 115200, ReadTimeout: time.Millisecond * 5000}
-    s, err := serial.OpenPort(c)
-    if err != nil {
-        log.Fatal(err)
-        return
-    }
-    log.Printf("Serial port opened %v", s)
-    n, err := s.Write([]byte("run                 "))
-    if err != nil {
-        log.Fatal(err)
-        return
-    }
-    log.Printf("Sent 'run' command, n=%v", n)
 
-    buf := make([]byte, 256)
-
-    for {
-        n, err = s.Read(buf)
-        if err != nil {
-            log.Printf("err != nil from s.Read: %v", err)
-            break
-        }
-        log.Printf("read got n=%v", n)
-        nsmall := 10
-        if n < 10 {
-            nsmall = n
-        }
-        log.Print(buf[0:nsmall])
-    }
-    log.Printf("Exiting program")
 }
